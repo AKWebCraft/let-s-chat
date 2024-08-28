303587c01e720135761c68711d6de264884d60a3
Arslan Khalil <arslankhalil660@gmail.com>
2024-08-27 23:09:00 -0700

    time color change

--- frontend/src/components/Messages/Message.jsx
+++ frontend/src/components/Messages/Message.jsx
@@ -22,7 +22,7 @@ const Message = ({ message }) => {
       <div className={`chat-bubble text-white ${bubbleBgColor}`}>
         {message.message}
       </div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-white">
         {formattedTime}
       </div>
     </div>
